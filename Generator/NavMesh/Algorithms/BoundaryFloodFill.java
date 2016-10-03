e903e9343128764408930c48abd47ff6d1892f0c
Bas <bnoorlander@hotmail.com>
2016-10-03 21:19:24 +0200

    Ignore 1x1 holes in the boundary flood fill algorithm

--- Generator/NavMesh/Algorithms/BoundaryFloodFill.java
+++ Generator/NavMesh/Algorithms/BoundaryFloodFill.java
@@ -1,5 +1,6 @@
 package scripts.AdvancedWalking.Generator.NavMesh.Algorithms;
 
+import org.tribot.api2007.PathFinding;
 import org.tribot.api2007.types.RSTile;
 import scripts.AdvancedWalking.Game.World.Direction;
 import scripts.AdvancedWalking.Generator.Generator;
@@ -48,17 +49,39 @@ public class BoundaryFloodFill {
                 if ((chkTile = generator.findTile(adjTile)) == null)
                     chkTile = new MeshTile(adjTile);
 
-                if (tiles.contains(chkTile)) {
-                    // this adjacent tile is inside the shape
-                 //   tileStack.push(chkTile);
-                } else {
+                if (!tiles.contains(chkTile)) {
                     // this adjacent is NOT in our shape, so the current tile is a boundary tile.
-                    isBoundary = true;
+
+                    // But we check for 1x1 holes first.
+                    boolean allInShape = true;
+                    // Check if its a single hole.
+                    if (!PathFinding.isTileWalkable(chkTile)) {
+
+                        for (Direction dir : Direction.getAllCardinal()) {
+
+                            RSTile adjTile2 = chkTile.getAdjacentTile(dir);
+
+                            MeshTile chkTile2;
+                            if ((chkTile2 = generator.findTile(adjTile2)) == null)
+                                chkTile2 = new MeshTile(adjTile2);
+
+                            if (!tiles.contains(chkTile2)) {
+                                allInShape = false;
+                                break;
+                            }
+                        }
+                    } else {
+                        allInShape = false;
+                    }
+
+                    if (!allInShape)
+                        isBoundary = true;
                 }
             }
 
-            if (isBoundary)
+            if (isBoundary) {
                 result.add(curTile);
+            }
 
             closedList.add(curTile);
         }
